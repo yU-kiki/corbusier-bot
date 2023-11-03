d61a287f67be42f23eab45af6a8904750c253a45
yU-kiki <yuki521ike@gmail.com>
2023-11-04 00:55:31 +0900

    指定したシートへ書き込み

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const { google } = require('googleapis');
 const line = require('@line/bot-sdk');
 const { computerVisionClient, convertOCRTextToJSON, readTextFromBuffer, extractTextArrayFromReadResults } = require('./utilities');
 
@@ -8,12 +9,67 @@ const config = {
   channelSecret: process.env.LINE_CHANNEL_SECRET,
 };
 
+const keyFilePath = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+const spreadsheetId = process.env.SPREADSHEET_ID;
+const sheetName = '履歴テスト';
+
 const app = express();
 
 app.use(express.json({ verify: (req, _, buf) => req.rawBody = buf }));
 app.use(express.urlencoded({ extended: true }));
 
 const client = new line.Client(config);
+
+const auth = new google.auth.GoogleAuth({
+  keyFile: keyFilePath,
+  scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+});
+
+const googleSheetsInstance = google.sheets({ version: 'v4', auth });
+
+async function writeToSheet(jsonResult) {
+  const authClient = await auth.getClient();
+
+  const getRows = await googleSheetsInstance.spreadsheets.values.get({
+    spreadsheetId,
+    range: `${sheetName}!B:B`,
+  });
+
+  const firstEmptyRow = getRows.data.values ? getRows.data.values.length + 1 : 1;
+
+  await googleSheetsInstance.spreadsheets.values.update({
+    spreadsheetId,
+    range: `${sheetName}!B${firstEmptyRow}:G${firstEmptyRow}`,
+    valueInputOption: 'USER_ENTERED',
+    resource: {
+      values: [
+        [
+          jsonResult['日時'],
+          `=if(${sheetName}!B${firstEmptyRow}="","", MONTH(${sheetName}!B${firstEmptyRow}))`, // 月
+          'LINE送信者の名前',
+          jsonResult['合計金額'],
+          jsonResult['項目'],
+          `${jsonResult['明細'].slice(0, 3).map(item => item['商品名']).join('、')}など`, // 内容
+        ],
+      ],
+    },
+  });
+}
+
+async function replyToLine(event, jsonResult) {
+  await writeToSheet(jsonResult);
+  const replyMessage = `以下の内容で登録しました。\n` +
+    `【店名】${jsonResult['店名']}\n` +
+    `【日時】${jsonResult['日時']}\n` +
+    `【金額】${jsonResult['合計金額']}円\n` +
+    `【項目】${jsonResult['項目']}\n` +
+    `【内容】${jsonResult['明細'].slice(0, 3).map(item => item['商品名']).join('、')} など`;
+
+  await client.replyMessage(event.replyToken, {
+    type: 'text',
+    text: replyMessage
+  });
+}
 
 async function handleImageMessage(event) {
   try {
@@ -34,12 +90,13 @@ async function handleImageMessage(event) {
     const textArray = await extractTextArrayFromReadResults(readResults);
     const textResult = textArray.join('\n');
 
-    const jsonResult = await convertOCRTextToJSON(textResult);
-    console.log(jsonResult);
-
-    // 応答をLINEに送信するなどの処理をここに追加
-    // ...
-
+    const jsonResultString = await convertOCRTextToJSON(textResult);
+    try {
+      const jsonResult = JSON.parse(jsonResultString);
+      await replyToLine(event, jsonResult);
+    } catch (e) {
+      console.error("JSON解析エラー:", e);
+    }
   } catch (err) {
     console.error(err);
   }
