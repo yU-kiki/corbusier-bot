fab101235208ec20dd9e90a203079d7deb51af6c
yU-kiki <yuki521ike@gmail.com>
2024-03-23 20:47:31 +0900

    google-spreadsheetの認証プロセスの修正

--- spreadsheetService.js
+++ spreadsheetService.js
@@ -7,14 +7,13 @@ const serviceAccountAuth = new JWT({
   scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
 
+const doc = new GoogleSpreadsheet(
+  process.env.GOOGLE_PURCHASE_SPREADSHEET_ID ?? '',
+  serviceAccountAuth,
+);
+
 async function saveToSpreadSheet(jsonResult) {
   try {
-    const doc = new GoogleSpreadsheet(process.env.SPREADSHEET_ID ?? '');
-    await doc.useServiceAccountAuth({
-      client_email: process.env.GOOGLE_CLIENT_EMAIL,
-      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    });
-
     await doc.loadInfo();
     const sheet = doc.sheetsByIndex[0];
 
