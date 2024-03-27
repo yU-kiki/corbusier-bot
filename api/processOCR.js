fb1233789e6b07920cca554267d29035abaa2bf3
yU-kiki <yuki521ike@gmail.com>
2024-03-27 14:25:44 +0900

    指示をなるべく簡潔に修正

--- api/processOCR.js
+++ api/processOCR.js
@@ -8,18 +8,14 @@ const app = express();
 app.use(express.json({ verify: (req, _, buf) => req.rawBody = buf }));
 
 app.post('/processOCR', async (req, res) => {
-  console.log(`processOCR handling start: ${new Date().toISOString()}`);
 
   try {
     const { textResult, event, userName } = req.body;
-    console.log('convertOCRTextToJSON\n');
-    console.log(`start: ${new Date().toISOString()}`);
+    const start = new Date();
     const jsonResultString = await convertOCRTextToJSON(textResult);
-    console.log(`end: ${new Date().toISOString()}`);
-    console.log('JSON.parse')
-    console.log(`start: ${new Date().toISOString()}`);
+    const end = new Date();
+    console.log(`convertOCRTextToJSON Processing Time: ${end - start}ms`);
     const jsonResult = JSON.parse(jsonResultString);
-    console.log(`end: ${new Date().toISOString()}`);
 
     const nameMappings = {
       'Yuki Ikeda': 'YUKI',
@@ -30,14 +26,10 @@ app.post('/processOCR', async (req, res) => {
     jsonResult['ユーザー名'] = userName;
     jsonResult['変更ユーザー名'] = userReName;
 
-    console.log('replyToLine]\n')
-    console.log(`start: ${new Date().toISOString()}`);
     await replyToLine(event, jsonResult);
-    console.log(`start: ${new Date().toISOString()}`);
     res.status(200).json({ message: 'OCR processing and line reply completed' });
   } catch (err) {
     console.error(`Error in Process OCR: ${err}`);
-    console.log(`Process OCR end with error: ${new Date().toISOString()}`);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
